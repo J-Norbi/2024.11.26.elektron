e8f22cbef75f4e5d458467c137c9dbc25ee5c674
jambo <jambo@LAPTOP-TOR02GNI>
2024-11-26 21:55:10 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace _2024._11._26.elektron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.felButton = new System.Windows.Forms.Button();
+            this.leButton = new System.Windows.Forms.Button();
+            this.kijelzo = new System.Windows.Forms.Label();
+            this.elektron = new System.Windows.Forms.PictureBox();
+            this.akkumulator = new System.Windows.Forms.PictureBox();
+            this.kapcsolo = new System.Windows.Forms.PictureBox();
+            this.FentiKabel = new System.Windows.Forms.PictureBox();
+            this.LentiKabel = new System.Windows.Forms.PictureBox();
+            this.BalKabel = new System.Windows.Forms.PictureBox();
+            this.JobbKabel = new System.Windows.Forms.PictureBox();
+            this.villanykorte = new System.Windows.Forms.PictureBox();
+            this.result = new System.Windows.Forms.Label();
+            this.sargaKorte = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.elektron)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.akkumulator)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kapcsolo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FentiKabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LentiKabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BalKabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JobbKabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.villanykorte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sargaKorte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // felButton
+            // 
+            this.felButton.Location = new System.Drawing.Point(386, 128);
+            this.felButton.Name = "felButton";
+            this.felButton.Size = new System.Drawing.Size(42, 23);
+            this.felButton.TabIndex = 9;
+            this.felButton.Text = "/\\";
+            this.felButton.UseVisualStyleBackColor = true;
+            // 
+            // leButton
+            // 
+            this.leButton.Location = new System.Drawing.Point(386, 154);
+            this.leButton.Name = "leButton";
+            this.leButton.Size = new System.Drawing.Size(42, 23);
+            this.leButton.TabIndex = 10;
+            this.leButton.Text = "\\/";
+            this.leButton.UseVisualStyleBackColor = true;
+            // 
+            // kijelzo
+            // 
+            this.kijelzo.AutoSize = true;
+            this.kijelzo.Location = new System.Drawing.Point(393, 102);
+            this.kijelzo.Name = "kijelzo";
+            this.kijelzo.Size = new System.Drawing.Size(26, 13);
+            this.kijelzo.TabIndex = 11;
+            this.kijelzo.Text = "12V";
+            // 
+            // elektron
+            // 
+            this.elektron.BackColor = System.Drawing.Color.Yellow;
+            this.elektron.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.elektron.Location = new System.Drawing.Point(642, 250);
+            this.elektron.Name = "elektron";
+            this.elektron.Size = new System.Drawing.Size(6, 6);
+            this.elektron.TabIndex = 12;
+            this.elektron.TabStop = false;
+            // 
+            // akkumulator
+            // 
+            this.akkumulator.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("akkumulator.BackgroundImage")));
+            this.akkumulator.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.akkumulator.Location = new System.Drawing.Point(587, 276);
+            this.akkumulator.Name = "akkumulator";
+            this.akkumulator.Size = new System.Drawing.Size(67, 50);
+            this.akkumulator.TabIndex = 8;
+            this.akkumulator.TabStop = false;
+            // 
+            // kapcsolo
+            // 
+            this.kapcsolo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.kapcsolo.Location = new System.Drawing.Point(355, 92);
+            this.kapcsolo.Name = "kapcsolo";
+            this.kapcsolo.Size = new System.Drawing.Size(100, 94);
+            this.kapcsolo.TabIndex = 6;
+            this.kapcsolo.TabStop = false;
+            // 
+            // FentiKabel
+            // 
+            this.FentiKabel.BackColor = System.Drawing.Color.Blue;
+            this.FentiKabel.Location = new System.Drawing.Point(162, 140);
+            this.FentiKabel.Name = "FentiKabel";
+            this.FentiKabel.Size = new System.Drawing.Size(484, 2);
+            this.FentiKabel.TabIndex = 5;
+            this.FentiKabel.TabStop = false;
+            // 
+            // LentiKabel
+            // 
+            this.LentiKabel.BackColor = System.Drawing.Color.Red;
+            this.LentiKabel.Location = new System.Drawing.Point(162, 274);
+            this.LentiKabel.Name = "LentiKabel";
+            this.LentiKabel.Size = new System.Drawing.Size(437, 2);
+            this.LentiKabel.TabIndex = 4;
+            this.LentiKabel.TabStop = false;
+            // 
+            // BalKabel
+            // 
+            this.BalKabel.BackColor = System.Drawing.Color.Red;
+            this.BalKabel.Location = new System.Drawing.Point(162, 140);
+            this.BalKabel.Name = "BalKabel";
+            this.BalKabel.Size = new System.Drawing.Size(2, 136);
+            this.BalKabel.TabIndex = 2;
+            this.BalKabel.TabStop = false;
+            // 
+            // JobbKabel
+            // 
+            this.JobbKabel.BackColor = System.Drawing.Color.Blue;
+            this.JobbKabel.Location = new System.Drawing.Point(644, 140);
+            this.JobbKabel.Name = "JobbKabel";
+            this.JobbKabel.Size = new System.Drawing.Size(2, 136);
+            this.JobbKabel.TabIndex = 0;
+            this.JobbKabel.TabStop = false;
+            // 
+            // villanykorte
+            // 
+            this.villanykorte.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("villanykorte.BackgroundImage")));
+            this.villanykorte.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.villanykorte.Location = new System.Drawing.Point(129, 69);
+            this.villanykorte.Name = "villanykorte";
+            this.villanykorte.Size = new System.Drawing.Size(68, 85);
+            this.villanykorte.TabIndex = 7;
+            this.villanykorte.TabStop = false;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(516, 37);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 13);
+            this.result.TabIndex = 13;
+            this.result.Text = "Electrons passed: 0";
+            // 
+            // sargaKorte
+            // 
+            this.sargaKorte.BackgroundImage = global::_2024._11._26.elektron.Properties.Resources.sarga_korte;
+            this.sargaKorte.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.sargaKorte.Location = new System.Drawing.Point(129, 69);
+            this.sargaKorte.Name = "sargaKorte";
+            this.sargaKorte.Size = new System.Drawing.Size(68, 85);
+            this.sargaKorte.TabIndex = 14;
+            this.sargaKorte.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.leButton);
+            this.Controls.Add(this.felButton);
+            this.Controls.Add(this.kijelzo);
+            this.Controls.Add(this.kapcsolo);
+            this.Controls.Add(this.elektron);
+            this.Controls.Add(this.akkumulator);
+            this.Controls.Add(this.FentiKabel);
+            this.Controls.Add(this.LentiKabel);
+            this.Controls.Add(this.BalKabel);
+            this.Controls.Add(this.JobbKabel);
+            this.Controls.Add(this.sargaKorte);
+            this.Controls.Add(this.villanykorte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.elektron)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.akkumulator)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kapcsolo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FentiKabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LentiKabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BalKabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JobbKabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.villanykorte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sargaKorte)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox JobbKabel;
+        private System.Windows.Forms.PictureBox BalKabel;
+        private System.Windows.Forms.PictureBox LentiKabel;
+        private System.Windows.Forms.PictureBox FentiKabel;
+        private System.Windows.Forms.PictureBox kapcsolo;
+        private System.Windows.Forms.PictureBox villanykorte;
+        private System.Windows.Forms.PictureBox akkumulator;
+        private System.Windows.Forms.Button felButton;
+        private System.Windows.Forms.Button leButton;
+        private System.Windows.Forms.Label kijelzo;
+        private System.Windows.Forms.PictureBox elektron;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.PictureBox sargaKorte;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace _2024._11._26.elektron
+{
+    public partial class Form1 : Form
+    {
+        Timer electronTimer = new Timer();
+        //int voltSpeed = 2;
+        int voltSize = 12;
+        int iranyX = 0;      // 1 - jobbra; 0 - fel vagy le; -1 - balra
+        int iranyY = -1;      // 1 - le; 0 - jobbra vagy balra; -1 - fel
+        int electronResults = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Voltage";
+            Start();
+        }
+        void Start()
+        {
+            electronTimer.Interval = 30;
+            AddEvents();
+            electronTimer.Start();
+            electronTimer.Tick += ElectronSpeed;
+        }
+        void AddEvents()
+        {
+            felButton.Click += upVoltage;
+            leButton.Click += downVoltage;
+        }
+        void upVoltage(object s, EventArgs e)
+        {
+            if (voltSize < 24)
+            {
+                voltSize += 1;
+                kijelzo.Text = voltSize.ToString() + "V";
+            }
+        }
+        void downVoltage(object s, EventArgs e)
+        {
+            if (voltSize > 1)
+            {
+                voltSize -= 1;
+                kijelzo.Text = voltSize.ToString() + "V";
+            }
+        }
+        void ElectronSpeed(object s, EventArgs e)
+        {
+            //  felfelé megy
+            if (iranyX == 0 && iranyY == -1)
+            {
+                if (elektron.Top > FentiKabel.Top - 2)
+                {
+                    elektron.Top -= voltSize;
+                }
+                else
+                {
+                    elektron.Top = FentiKabel.Top - 2;
+                    iranyX = -1;
+                    iranyY = 0;
+                }
+
+            }
+            //  lefelé megy
+            else if (iranyX == 0 && iranyY == 1)
+            {
+                if (elektron.Top < LentiKabel.Bottom - elektron.Height + 2)
+                {
+                    elektron.Top += voltSize;
+                }
+                else
+                {
+                    elektron.Top = LentiKabel.Bottom - elektron.Height + 2;
+                    iranyX = 1;
+                    iranyY = 0;
+                }
+            }
+            //  balra megy
+            else if (iranyX == -1 && iranyY == 0)
+            {
+                if (elektron.Left > FentiKabel.Left - 2)
+                {
+                    elektron.Left -= voltSize;
+                }
+                else
+                {
+                    elektron.Left = FentiKabel.Left - 2;
+                    iranyX = 0;
+                    iranyY = 1;
+                    electronResults += 1;
+                    result.Text = $"Electrons passed: {electronResults}";
+                }
+            }
+            //  jobbra megy
+            else if (iranyX == 1 && iranyY == 0)
+            {
+                if (elektron.Left < JobbKabel.Right - elektron.Width + 2)
+                {
+                    elektron.Left += voltSize;
+                    if (elektron.Left >= LentiKabel.Right - 2)
+                    {
+                        elektron.Visible = false;
+                    }
+                }
+                else
+                {
+                    elektron.Left = JobbKabel.Right - elektron.Width + 2;
+                    iranyX = 0;
+                    iranyY = -1;
+                    elektron.Visible = true;
+                }
+            }
+        }
+
+    }
+}
